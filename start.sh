e31f810dfb182ac2912ec51e0d295ba1ec4242d9
Piyush Maharana <pshmaharana@gmail.com>
2026-05-29 20:31:45 +0530

    fix: limit celery concurrency to 1 to prevent memory crash

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Start Celery in the background
-celery -A celery_worker.celery_app worker -B --loglevel=info &
+# Start Celery in the background with only 1 worker to save memory
+celery -A celery_worker.celery_app worker -B -c 1 --loglevel=info &
+
 # Start Gunicorn in the foreground
 gunicorn app:app
